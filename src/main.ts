4ee36a017a76b5090b716921defb7738d79c356a
Loriyana Doncheva <loriyana.doncheva@accedia.com>
2019-12-01 20:32:30 +0200

    Extracted lazy loading demo in separate branch

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-// Change the import route here to change the current exercise
-import { AppModule } from './tasksCompleted/app.module';
 
 if (environment.production) {
   enableProdMode();
