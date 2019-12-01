73b13812054f738d8e3937217f3e1e8e8ccbbaeb
Loriyana Doncheva <loriyana.doncheva@accedia.com>
2019-12-01 19:54:30 +0200

    Deleted unused content from directives demo branch

--- Tasks/src/exercise3/pages/home-page/home-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'aw-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
-})
-export class HomePageComponent {}
