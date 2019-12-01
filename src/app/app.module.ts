ee641553919e7c9cb2d1428402f5fa37ad1f8b87
Loriyana Doncheva <loriyana.doncheva@accedia.com>
2019-12-01 21:15:56 +0200

    Set up initial state for pipes demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
-import { ImpovedHighlightDirective } from './directives/improved-highlight/improved-highlight.directive';
 import { UnlessDirective } from './directives/unless/unless.directive';
-import { FilterPipe } from './pipes/filter/filter.pipe';
 
 @NgModule({
    declarations: [
       AppComponent,
       PostsComponent,
       HighlightDirective,
-      ImpovedHighlightDirective,
-      UnlessDirective,
-      FilterPipe
+      UnlessDirective
    ],
    imports: [
       BrowserModule,
