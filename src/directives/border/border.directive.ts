edc3dcac9f68dfcf02fd9a3985b3f957cbcfd9ac
Loriyana Doncheva <loriyana.doncheva@accedia.com>
2019-12-08 14:47:08 +0200

    Moved border initiazlization to ngOnInit

--- src/directives/border/border.directive.ts
+++ src/directives/border/border.directive.ts
@@ -6,11 +6,14 @@ import { Directive, OnInit } from '@angular/core';
 export class AwBorderDirective implements OnInit {
   // TODO 2.2 - add Input parameter "color"
   display = 'block';
-  border = '3px solid #3257a8';
+  borderSize = '3px';
+  borderStyle = 'solid'
+  borderColor = '#3257a8'
 
   constructor(/** TODO 2.1 - add Renderer2 and ElementRef*/) { }
 
   ngOnInit() {
+    const border = `${this.borderSize} ${this.borderStyle} ${this.borderColor}`;
     // TODO 2.1 - set display and border styles
   }
 }
